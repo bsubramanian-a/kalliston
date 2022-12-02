bb489520246bb27e34c9850f4f6e062ce4de315b
C_Manoj_Kumar <manoj.c@crtvecode.in>
2022-12-02 13:25:26 +0530

    Added new tables

---
+++ api/server/src/migrations/20221202071459-create-package-details.js
@@ -0,0 +1,53 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('PackageDetails', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      short_description: {
+        allowNull: true,
+        type: Sequelize.BYTEA
+      },
+      long_description: {
+        allowNull: true,
+        type: Sequelize.BYTEA
+      },
+      access_link: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      program_type: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      offer_type: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      type: {
+        type: Sequelize.ENUM('client', 'coach', 'admin'),
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('PackageDetails');
+  }
+};
---
+++ api/server/src/migrations/20221202072530-create-package-price.js
@@ -0,0 +1,37 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('PackagePrices', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      term: {
+        type: Sequelize.ENUM('monthly', 'yearly'),
+        allowNull: false,
+      },
+      price: {
+        type: Sequelize.REAL,
+        allowNull: false,
+      },
+      package_details: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('PackagePrices');
+  }
+};
