2875b583e3a95c43bd89a478f8a61846691e2f6e
Rajavel Balasubramanian <bsubramanian.a@gmail.com>
2023-01-17 17:21:46 +0530

    changed the variable

--- api/server/controllers/UserController.js
+++ api/server/controllers/UserController.js
@@ -9,7 +9,7 @@ const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.PASSWORD
+    pass: process.env.EMAIL_PASSWORD
   }
 });
 
